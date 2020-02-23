ec9935cda40adca2764bb60f9e96cbf443acb261
Anni Chen <anni.chen@duke.edu>
2020-02-22 21:28:13 -0500

    Second commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './assets/style.css';
+import quizService from "./quizService";
+import QuestionBox from "./components/QuestionBox";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
+
+class Quizbee extends React.Component{
+    state = {
+        questionBank: [],
+        score: 0,
+        responses: 0
+    };
+    getQuestions = () => {
+        quizService().then(question => {
+            this.setState({
+                questionBank : question
+            });
+        });
+    };
+    computeAnswer = (answer, correctAnswer) => {
+        if (answer === correctAnswer) {
+            this.setState({
+                score: this.score + 1
+            });
+        }
+        this.setState({
+            responses: this.responses < 5 ? this.state.responses + 1 : 5
+        });
+    };
+    componentDidMount(){
+        this.getQuestions();
+    }
+    render(){   
+        return (
+            <div className = "container">
+                <div className = "title">Watering trees!</div>
+                {this.state.questionBank.length > 0 
+                && this.state.responses < 5
+                && this.state.questionBank.map(
+                    ({question, answers, correct, questionId}) => (
+                        <QuestionBox 
+                        question= {question} 
+                        options = {answers} 
+                        key={questionId}
+                        selected = {answer => this.computeAnswer(answer, correct)}
+                    />)
+                ) }
+
+                {/* {this.state.responses === 5 ? (<h2>{this.state.score}</h2>): null} */}
+            </div>
+        );
+    };
+};
+
+ReactDOM.render(<Quizbee />, document.getElementById('root'));
