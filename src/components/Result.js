7b541b14a4a43a57b0b667c6d62d8e26238f74e9
Anni Chen <anni.chen@duke.edu>
2020-02-23 09:26:13 -0500

    Pre-final

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,8 +1,21 @@
 import React from "react";
+import tree from "../assets/tree.png";
+import sapling from "../assets/sapling.png"
 
 const Result = ({score, playAgain}) => (
     <div className="score-board">
-        <div className = "score">You scored {score}. You earned a tree!</div>
+        
+        <div className ="tree"><section>
+            <img class ="bottom" src = {tree} alt="big tree"/>
+            <img class = "top" src = {sapling} alt = "small tree"/>
+        </section></div>
+
+        <div className = "score">You scored ${score}. You earned a tree!</div>
+            
+        
+
+
+
         <button className = "playBtn" onClick={playAgain}>
             Try again!
         </button>
